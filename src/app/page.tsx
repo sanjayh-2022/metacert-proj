21796139546a8b2c6e13dd061f1a1fd63aaf79f3
hsanjay <1ds22cy017@dsce.edu.in>
2025-05-06 12:00:00 +0530

    MetaCert deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
 'use client'
 import Navbar from './components/Navbar'
 import Body from './components/Body'
-import { useSession } from 'next-auth/react'
-
-function HomeContent() {
-  const { data: session, status } = useSession()
-
-  if (status === 'loading') {
-    return (
-      <div className="min-h-screen bg-stone-700 flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500 mx-auto mb-4"></div>
-          <p className="text-white text-lg">Loading...</p>
-        </div>
-      </div>
-    )
-  }
-
-  return (
-    <>
-      <Navbar />
-      <Body />
-    </>
-  )
-}
+import AuthGuard from './components/AuthGuard'
 
 export default function Home() {
-  return <HomeContent />
+  return (
+    <AuthGuard>
+      <Navbar />
+      <Body />
+    </AuthGuard>
+  )
 }
